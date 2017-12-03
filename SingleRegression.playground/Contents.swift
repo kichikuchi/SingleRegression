d69a1d9a5d716b849198466c5297438df50cd9f5
kichikuchi <kikuchi0111@gmail.com>
2017-12-03 23:35:56 +0900

    add sum and product

--- SingleRegression.playground/Contents.swift
+++ SingleRegression.playground/Contents.swift
@@ -1,5 +1,25 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+struct Matrix {
+    let element: [Double]
+    func sum() -> Double {
+        return element.reduce(0.0) { $0 + $1 }
+    }
+}
+
+func *(lhs: Matrix, rhs: Matrix) -> Matrix {
+    if lhs.element.count != rhs.element.count {
+        // 今回は要素数が違う場合を考慮しません
+        assertionFailure()
+    }
+    
+    var array: [Double] = []
+    for i in 0..<lhs.element.count {
+        let prod = lhs.element[i] * rhs.element[i]
+        array.append(prod)
+    }
+    
+    return Matrix(element: array)
+}
