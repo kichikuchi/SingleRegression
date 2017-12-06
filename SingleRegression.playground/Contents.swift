610ec510359072e7f62ef563b02685c74ff92019
kichikuchi <kikuchi0111@gmail.com>
2017-12-06 23:54:21 +0900

    予測値を求めるところまで

--- SingleRegression.playground/Contents.swift
+++ SingleRegression.playground/Contents.swift
@@ -1,10 +1,12 @@
 //: Playground - noun: a place where people can play
 
+import UIKit
 import Foundation
+import XCPlayground
 
 struct Matrix {
     
-    let element: [Double]
+    var element: [Double]
     
     func sum() -> Double {
         return element.reduce(0.0) { $0 + $1 }
@@ -12,6 +14,10 @@ struct Matrix {
     
     func mean() -> Double {
         return sum() / Double(element.count)
+    }
+    
+    mutating func difference(_ diff: Double) {
+        element = element.map { $0 - diff }
     }
 }
 
@@ -29,3 +35,26 @@ func *(lhs: Matrix, rhs: Matrix) -> Matrix {
     
     return Matrix(element: array)
 }
+
+// 学習に用いるサンプルデータ
+let x: [Double] = [40, 38, 36, 35, 32, 45, 55, 32, 28, 41, 44, 50, 43, 44, 31]
+let y: [Double] = [137000, 93000, 95000, 97000, 85000, 140000, 213000, 82000, 75000, 103000, 110000, 195000, 124000, 99000, 72000]
+
+var xMatrix = Matrix(element: x)
+var yMatrix = Matrix(element: y)
+
+// 中心化
+let bar_x = xMatrix.mean()
+xMatrix.difference(bar_x)
+
+let bar_y = yMatrix.mean()
+yMatrix.difference(bar_y)
+
+// パラメータを求める
+let xx = xMatrix * xMatrix
+let xy = xMatrix * yMatrix
+let a = xy.sum() / xx.sum()
+
+// 予測値を求める
+let predict = a * (40 - bar_x) + bar_y
+print(predict)
